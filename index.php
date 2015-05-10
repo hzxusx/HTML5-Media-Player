0aa5e794359fb33768f06036e7103cf959a87ff4
MinonHeart <liuhao.mt@qq.com>
2015-05-10 14:25:06 +0800

    update index.php

--- index.php
+++ index.php
@@ -12,48 +12,45 @@
 </head>
 
 <body>
-    <?php include 'functions.php'; ?>
-
-    <h1><?php get_media_info('title'); ?></h1>
-
     <div id="wrapper">
+        <header id="header"><?php include 'functions.php'; get_media_info('title'); ?></header>
 
         <div id="container">
 
-            <video id="media-source" width="640" height="360" poster="<?php get_media_info('poster'); ?>" preload>
-                <source src="<?php get_media_info('source'); ?>" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"' />
-                Your browser does not support the video tag.
+            <video id="media-source" width="640" height="360" poster="<?php get_media_info('poster'); ?>" preload="auto">
+                <source src="<?php get_media_info('source'); ?>" type='video/mp4; codecs="avc1.42E01E, mp4a.40.2"'>
+                Current browser does not support the video tag.
             </video>
 
             <div id="html5-controls">
 
-                <div id="play-button" class="circle">
+                <section id="play-button" class="circle">
                     <div class="play"></div>
                     <div class="pause">
                         <div class="bar-1"></div>
                         <div class="bar-2"></div>
                     </div>
-                </div>
+                </section>
 
-                <div id="progress-area">
+                <section id="progress-area">
                     <div class="buffer"></div>
                     <div class="complete"></div>
                     <div class="dropper"></div>
-                </div>
+                </section>
 
-                <div id="time-area">
+                <section id="time-area">
                     <span class="current">0:00</span>
                     <span> / </span>
                     <span class="total">0:00</span>
-                </div>
+                </section>
 
-                <div id="zoom-area" class="circle">
+                <section id="zoom-area" class="circle">
                     <div class="icon">
                         <div class="button"></div>
                     </div>
-                </div>
+                </section>
 
-                <div id="speaker-area">
+                <section id="speaker-area">
                     <div id="speaker-button" class="circle">
                         <div class="icon-1"></div>
                         <div class="icon-2"></div>
@@ -64,14 +61,14 @@
                             <div class="slider"></div>
                         </div>
                     </div>
-                </div>
+                </section>
 
             </div>
 
             <div class="clear"></div>
         </div>
 
-        <div id="footer"></div>
+        <footer id="footer"></footer>
     </div>
 
     <script src="script.js"></script>
